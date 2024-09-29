d3a5531f57aaac699ad870b300ba4a72e8fa87c6
Thales Meier <thalesmeier@Thales-MacBook-Air.local>
2024-09-29 01:24:00 -0300

    added validations to sensor

--- src/simulator/models.py
+++ src/simulator/models.py
@@ -1,5 +1,5 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel, field_validator
+from pydantic_core import PydanticCustomError
 
 class Machine(BaseModel):
     name: str = "General Machine"
@@ -7,10 +7,36 @@ class Machine(BaseModel):
 
 # TODO: Maybe turn all fields into Pydantic's Field
 class Sensor(Machine):
-    min_reading: float  # TODO: Validate min_reading < max_reading
+    min_reading: float
     max_reading: float
-    steps: float  # TODO: Validate steps < (max_reading - min_reading)
+    steps: float
     unit_of_measure: str
+
+    @field_validator('min_reading')
+    @classmethod
+    def validate_min_reading(cls, v: float) -> float:
+        if v > cls.max_reading:
+            raise PydanticCustomError(
+                'interval_error',
+                'min_reading must be less than max_reading',
+                {'min_reading': v,
+                 'max_reading': cls.max_reading},
+            )
+        return v
+
+    @field_validator('steps')
+    @classmethod
+    def validate_steps(cls, v: float) -> float:
+        if v > (cls.max_reading - cls.min_reading):
+            raise PydanticCustomError(
+                'interval_error',
+                'steps must be less than the interval set for the readings',
+                {'steps': v,
+                 'interval_distance': (cls.max_reading - cls.min_reading)}
+            )
+        return v
+
+    # TODO: Test all validations
 
 
 class Switch(Machine):
