8d7d209d1109c037b7fbc86e6ddfd6ece9643b78
Thales Meier <thalesmeier@Thales-MacBook-Air.local>
2024-09-29 00:56:17 -0300

    refactor to start using pydantic

--- src/simulator/models.py
+++ src/simulator/models.py
@@ -1,14 +1,25 @@
-class BaseMachine():
-    def __init__(self, model, name):
-        self.model = model
-        self.name = name
-
-    def __str__(self):
-        return "{} - {}".format(self.model, self.name)
+from pydantic import BaseModel
 
 
-class BaseSensor(BaseMachine):
-    def __init__(self, min_reading: float, max_reading: float, steps: float):
-        self.min_reading = min_reading
-        self.max_reading = max_reading
-        self.steps = steps
+class Machine(BaseModel):
+    name: str = "General Machine"
+
+
+# TODO: Maybe turn all fields into Pydantic's Field
+class Sensor(Machine):
+    min_reading: float  # TODO: Validate min_reading < max_reading
+    max_reading: float
+    steps: float  # TODO: Validate steps < (max_reading - min_reading)
+    unit_of_measure: str
+
+
+class Switch(Machine):
+    pass
+
+
+class BinarySwitch(Switch):
+    pass
+
+
+class ValueSwitch(Switch):
+    pass
